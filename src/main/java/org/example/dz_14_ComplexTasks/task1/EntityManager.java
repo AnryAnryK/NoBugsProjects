a362f4f1a9261515c7bc58c16184d6f1a830ebfc
AnryAnryK <andkay@yandex.ru>
2026-06-11 14:36:26 +0300

    2026.06.11_ dz_14_Комплексные задачи по модулю Экспресс-Java _ все задачи с Task1 по Task6 сделаны

--- src/main/java/org/example/dz_14_ComplexTasks/task1/EntityManager.java
+++ src/main/java/org/example/dz_14_ComplexTasks/task1/EntityManager.java
@@ -1,19 +1,48 @@
 package org.example.dz_14_ComplexTasks.task1;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
-public class EntityManager <T> {
-	private  final List<T> list1 = new ArrayList<>();
+public class EntityManager<T> {
 
-	public synchronized T addUser(T user){
+	private final CopyOnWriteArrayList<T> list1 = new CopyOnWriteArrayList<>();
+
+
+	public synchronized T addUser(T user) {
 		list1.add(user);
 		return user;
 	}
 
-	public synchronized List<T> getAllUsers(){
-		return new ArrayList<>(list1);
+	public synchronized List<T> getAllUsers() {
+		return List.copyOf(list1);
+	}
+
+	public synchronized boolean removeUser(T user) {
+		if (!list1.isEmpty()) {
+			return list1.remove(user);
+		}
+		return false;
+	}
+
+
+	public List<T> filterByAge(int minAge, int maxAge) {
+		return list1.stream()
+				.filter(e -> e instanceof User)
+				.map(e -> (User) e)
+				.filter(user -> user.getAge() >= minAge &&
+						user.getAge() <= maxAge)
+				.map(user -> (T) user)
+				.collect(Collectors.toList());
+	}
+
+	public List<T> filterByName(String name) {
+		return list1.stream()
+				.filter(e -> e instanceof User)
+				.map(e -> (User) e)
+				.filter(user ->
+						user.getName().equalsIgnoreCase(name))
+				.map(user -> (T) user)
+				.collect(Collectors.toList());
 	}
 }
--- src/test/java/dz_14_ComplexTasks/task1/EntityManagerTest.java
+++ src/test/java/dz_14_ComplexTasks/task1/EntityManagerTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EntityManagerTest {
 
 	EntityManager<User> entityManager1 = new EntityManager<>();
-
 	List<User> allUsersList;
 
 	@Test
@@ -21,9 +19,67 @@ public class EntityManagerTest {
 	public void addUserPositiveTests() {
 		User user1 = new User(25, "Петя", true);
 		User actualAddUser1 = entityManager1.addUser(user1);
+		allUsersList = entityManager1.getAllUsers();
+		assertTrue(allUsersList.contains(user1));
+		assertSame(user1, actualAddUser1);
+	}
+
+	@Test
+	@DisplayName("Позитивные тесты: удаление User")
+	public void removeUserPositiveTests() {
+		User user1 = new User(25, "Петя", true);
+		User actualAddUser1 = entityManager1.addUser(user1);
 		assertSame(user1, actualAddUser1);
 
+		boolean actualRemovedUser = entityManager1.removeUser(user1);
 		allUsersList = entityManager1.getAllUsers();
-		System.out.println("Пользователь: " + user1.getName() + ", " + user1.getAge() + " - добавлен");
+		assertTrue(actualRemovedUser);
+		assertFalse(entityManager1.getAllUsers().contains(user1));
+	}
+
+	@Test
+	@DisplayName("Позитивные тесты: получения списка User в заданном возрастном диапазоне ")
+	public void getUserByAgeFilterPositiveTests() {
+		int minAge = 18;
+		int maxAge = 60;
+
+		User user1 = new User(25, "Петя", true);
+		entityManager1.addUser(user1);
+
+		User user2 = new User(17, "Вася", true);
+		entityManager1.addUser(user2);
+
+		allUsersList = entityManager1.filterByAge(minAge, maxAge);
+		assertEquals(1, allUsersList.size());
+		assertEquals("Петя", allUsersList.get(0).getName());
+		assertNotEquals("Вася", allUsersList.get(0).getName());
+	}
+
+	@Test
+	@DisplayName("Позитивные тесты: получения списка User, чьи имена соответствуют заданной строке")
+	public void getUserByNameFilterPositiveTests() {
+		User user1 = new User(25, "Петя", true);
+		entityManager1.addUser(user1);
+
+		User user2 = new User(17, "Вася", true);
+		entityManager1.addUser(user2);
+
+		List<User> actualUsersResultFilterByName = entityManager1.filterByName("Вася");
+		assertEquals(1, actualUsersResultFilterByName.size());
+	}
+
+	@Test
+	@DisplayName("Позитивные тесты: получения списка User со статусом активности - true")
+	public void getUserByFilterIsActivePositiveTests() {
+		User user1 = new User(25, "Петя", true);
+		entityManager1.addUser(user1);
+
+		User user2 = new User(17, "Вася", false);
+		entityManager1.addUser(user2);
+
+		boolean actualUsersByFilterIsActive1 = user1.isActive();
+		boolean actualUsersByFilterIsActive2 = user2.isActive();
+		assertTrue(actualUsersByFilterIsActive1);
+		assertFalse(actualUsersByFilterIsActive2);
 	}
 }
